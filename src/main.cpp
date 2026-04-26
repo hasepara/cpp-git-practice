40756553c7f861c11aa5475f655d3c9c0a33173a
長谷川智久 <hasegawatomohisa@HMac2024.local>
2026-04-26 14:59:20 +0900

    Feature change for rebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+<<<<<<< HEAD
 std::cout << "Main change before rebase" << std::endl;
+=======
+std::cout << "Rebase test feature" << std::endl;
+>>>>>>> 35e7ab5 (Feature change for rebase)
     return 0;
 }
